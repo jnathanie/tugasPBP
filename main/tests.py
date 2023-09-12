caf56f501aaa093e670a6408d2469d0f15422320
Jocelyn <jocelyn.nathanie@ui.ac.id>
2023-09-12 12:25:20 +0700

    second commit tugas2

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from main.views import show_main
 
 # Create your tests here.
+class TestUrls(TestCase):
+    def test_urls(self):
+        url = reverse("main:show_main")
+        self.assertEquals(resolve(url).func, show_main)
